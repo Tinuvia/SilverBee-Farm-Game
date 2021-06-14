12f7607b4361536f39fc03ed0f31c787a3b0c31f
TinuviaWV <71916978+Tinuvia@users.noreply.github.com>
2021-06-14 22:01:01 +0200

    UI tools part 6

--- Assets/Scripts/FarmManager.cs
+++ Assets/Scripts/FarmManager.cs
@@ -13,6 +13,14 @@ public class FarmManager : MonoBehaviour
     public Color buyColor = Color.green;
     public Color cancelColor = Color.red;
 
+    public bool isSelecting = false;
+    public int selectedTool = 0; // #1 water, #2 fertilizer, #3 buy plot
+
+    public Image[] buttonsImg;
+    public Sprite normalButton;
+    public Sprite selectedButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,18 +32,11 @@ public class FarmManager : MonoBehaviour
         // deselect if clicked while already selected
         if(selectPlant == newPlant)
         {
-            selectPlant.btnImage.color = buyColor;
-            selectPlant.btnTxt.text = "Buy";
-            selectPlant = null;
-            isPlanting = false;
+            CheckSelection();
         }
         else
         {
-            if(selectPlant != null)
-            {
-                selectPlant.btnImage.color = buyColor;
-                selectPlant.btnTxt.text = "Buy";
-            }
+            CheckSelection();
             selectPlant = newPlant;
             selectPlant.btnImage.color = cancelColor;
             selectPlant.btnTxt.text = "Cancel";
@@ -43,10 +44,50 @@ public class FarmManager : MonoBehaviour
         }
     }
 
+    // needs to be public since we call it from the button
+    public void SelectTool(int toolNumber)
+    {
+        if(toolNumber == selectedTool)
+        {
+            CheckSelection();
+        } else
+        {
+            // select tool number and check to see if anything else was also selected
+            CheckSelection();
+            isSelecting = true;
+            selectedTool = toolNumber;
+            buttonsImg[toolNumber - 1].sprite = selectedButton;
+        }
+    }
+
+    void CheckSelection()
+    {
+        if (isPlanting)
+        {
+            isPlanting = false;
+            if (selectPlant != null)
+            {
+                //deselecting
+                selectPlant.btnImage.color = buyColor;
+                selectPlant.btnTxt.text = "Buy";
+                selectPlant = null; 
+            }
+        }
+        if (isSelecting)
+        {
+            if (selectedTool > 0)
+            {
+                buttonsImg[selectedTool - 1].sprite = normalButton;
+            }
+            isSelecting = false;
+            selectedTool = 0;
+        }
+    }
+
     public void Transaction(int value)
     {
         money += value;
-        moneyTxt.text = "$" + money;
+        moneyTxt.text = "$" + money; 
     }
 
     // REFACTORING 
@@ -55,4 +96,5 @@ public class FarmManager : MonoBehaviour
     //  function UpdateSelectionButton(bool isBuying): 
     // --- selectPlant.btnImage.color = buyColor; cancelColor
     // --- selectPlant.btnTxt.text = "Buy";
+    // CheckSelection called outside of if statements since it's in both cases
 }
